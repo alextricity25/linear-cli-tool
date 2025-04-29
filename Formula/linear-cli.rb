ee24e4bdf04e544d1b58d64b0834c366970ed190
Alex Cantu <miguel.can2@gmail.com>
2025-04-29 09:03:20 -0500

    Update Homebrew formula with correct tarball URL, SHA256 and yarn installation

--- Formula/linear-cli.rb
+++ Formula/linear-cli.rb
@@ -1,15 +1,18 @@
 class LinearCli < Formula
   desc "CLI tool for interacting with Linear"
   homepage "https://github.com/alextricity25/linear-cli-tool"
-  url "https://github.com/alextricity25/linear-cli-tool-tool/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "REPLACE_WITH_ACTUAL_SHA_AFTER_RELEASE"
+  url "https://github.com/alextricity25/linear-cli-tool/archive/refs/tags/v0.1.0.tar.gz"
+  sha256 "996139893cfbbf695416a0415150e070497b4b8c685bb2655c510d6d9e106097"
   license "MIT"
 
   depends_on "node"
+  depends_on "yarn"
 
   def install
-    system "npm", "install", *Language::Node.std_npm_install_args(libexec)
-    bin.install_symlink Dir["#{libexec}/bin/*"]
+    system "yarn", "install"
+    system "yarn", "build"
+    libexec.install Dir["*"]
+    bin.install_symlink libexec/"dist/index.js" => "linear"
   end
 
   test do
